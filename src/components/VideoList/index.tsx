a0dc68145954ea206fc00a7866908c3ddc723759
RobertTheDev <robertthedev@gmail.com>
2023-12-27 00:00:57 +0000

    feat: mapped videos list component

--- src/components/VideoList/index.tsx
+++ src/components/VideoList/index.tsx
@@ -1,7 +1,19 @@
+import styles from "./styles.module.css";
+import videosData from "../../lib/data/videoData.json";
+import Image from "next/image";
+
 export default function VideoList() {
   return (
-    <div>
-      <p>Video List</p>
+    <div className={styles.videoList}>
+      {videosData.map((video) => {
+        return (
+          <div className={styles.videoContainer} key={video.id}>
+            <div className={styles.videoThumbnailImage}>
+              <Image fill src={video.thumbnail.src} alt={video.title} />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
