8ce8bcfeebd2c76dcba760554b45efb099fbfb23
RobertTheDev <robertthedev@gmail.com>
2023-12-27 03:18:42 +0000

    feat: formatted date

--- src/components/VideoList/index.tsx
+++ src/components/VideoList/index.tsx
@@ -2,6 +2,11 @@ import styles from "./styles.module.css";
 import videosData from "../../lib/data/videoData.json";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { formatDistanceToNowStrict } from "date-fns";
+
+const formatDate = (date: Date) => {
+  return formatDistanceToNowStrict(new Date(date), { addSuffix: true });
+};
 
 export default function VideoList() {
   const router = useRouter();
@@ -31,8 +36,8 @@ export default function VideoList() {
               <p>{video.channel.name}</p>
               <p>{video.title}</p>
               <p>{video.duration}</p>
-              <p>{video.views} views</p>
-              <p>{video.createdAt}</p>
+              <p>{video.views.toLocaleString()} views</p>
+              <p>{formatDate(new Date(video.createdAt))}</p>
             </div>
           </div>
         );
