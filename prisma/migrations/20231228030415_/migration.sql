c5814355afe9af897625c07c416adcc23e0568e1
RobertTheDev <robertthedev@gmail.com>
2023-12-28 03:05:04 +0000

    feat: installed and set up prisma

---
+++ prisma/migrations/20231228030415_/migration.sql
@@ -0,0 +1,157 @@
+-- CreateTable
+CREATE TABLE "Video" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "duration" INTEGER NOT NULL DEFAULT 0,
+    "title" TEXT NOT NULL,
+    "totalComments" INTEGER NOT NULL DEFAULT 0,
+    "totalDislikes" INTEGER NOT NULL DEFAULT 0,
+    "totalLikes" INTEGER NOT NULL DEFAULT 0,
+    "totalViews" INTEGER NOT NULL DEFAULT 0,
+    "thumbnailId" TEXT NOT NULL,
+    "channelId" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "VideoComment" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "content" TEXT NOT NULL,
+    "channelId" TEXT NOT NULL,
+    "videoId" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "VideoThumbnail" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "alt" TEXT,
+    "url" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Channel" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "subscribers" INTEGER NOT NULL DEFAULT 0,
+    "imageId" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "ChannelImage" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "alt" TEXT,
+    "url" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "VideoLike" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "channelId" TEXT NOT NULL,
+    "videoId" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "VideoSave" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "channelId" TEXT NOT NULL,
+    "videoId" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "VideoPlaylist" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "channelId" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "PlaylistedVideo" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "videoId" TEXT NOT NULL,
+    "videoPlaylistId" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Video_id_key" ON "Video"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Video_thumbnailId_key" ON "Video"("thumbnailId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VideoComment_id_key" ON "VideoComment"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VideoThumbnail_id_key" ON "VideoThumbnail"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Channel_id_key" ON "Channel"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Channel_imageId_key" ON "Channel"("imageId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ChannelImage_id_key" ON "ChannelImage"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VideoLike_id_key" ON "VideoLike"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VideoSave_id_key" ON "VideoSave"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VideoPlaylist_id_key" ON "VideoPlaylist"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PlaylistedVideo_id_key" ON "PlaylistedVideo"("id");
+
+-- AddForeignKey
+ALTER TABLE "Video" ADD CONSTRAINT "Video_thumbnailId_fkey" FOREIGN KEY ("thumbnailId") REFERENCES "VideoThumbnail"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Video" ADD CONSTRAINT "Video_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VideoComment" ADD CONSTRAINT "VideoComment_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VideoComment" ADD CONSTRAINT "VideoComment_videoId_fkey" FOREIGN KEY ("videoId") REFERENCES "Video"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Channel" ADD CONSTRAINT "Channel_imageId_fkey" FOREIGN KEY ("imageId") REFERENCES "ChannelImage"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VideoLike" ADD CONSTRAINT "VideoLike_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VideoLike" ADD CONSTRAINT "VideoLike_videoId_fkey" FOREIGN KEY ("videoId") REFERENCES "Video"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VideoSave" ADD CONSTRAINT "VideoSave_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VideoSave" ADD CONSTRAINT "VideoSave_videoId_fkey" FOREIGN KEY ("videoId") REFERENCES "Video"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VideoPlaylist" ADD CONSTRAINT "VideoPlaylist_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlaylistedVideo" ADD CONSTRAINT "PlaylistedVideo_videoId_fkey" FOREIGN KEY ("videoId") REFERENCES "Video"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlaylistedVideo" ADD CONSTRAINT "PlaylistedVideo_videoPlaylistId_fkey" FOREIGN KEY ("videoPlaylistId") REFERENCES "VideoPlaylist"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
