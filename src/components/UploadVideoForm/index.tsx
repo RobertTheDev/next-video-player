286678ba0cb63b7950b03f85b613f362362e6d82
RobertTheDev <robertthedev@gmail.com>
2023-12-28 12:31:58 +0000

    feat: created and imported upload video form

---
+++ src/components/UploadVideoForm/index.tsx
@@ -0,0 +1,10 @@
+export default function UploadVideoForm() {
+  return (
+    <div>
+      <form>
+        <input placeholder="Title" />
+        <button>Upload Video</button>
+      </form>
+    </div>
+  );
+}
--- src/pages/upload-video/index.tsx
+++ src/pages/upload-video/index.tsx
@@ -1,7 +1,10 @@
+import UploadVideoForm from "@/components/UploadVideoForm";
+
 export default function UploadVideo() {
   return (
     <div>
       <h1>Upload Video</h1>
+      <UploadVideoForm />
     </div>
   );
 }
