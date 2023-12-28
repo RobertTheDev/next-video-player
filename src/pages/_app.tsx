7a7ee7f5564d3b8753efa3cccc8ffc81afbb651f
RobertTheDev <robertthedev@gmail.com>
2023-12-28 11:07:01 +0000

    feat: installed and set up nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,23 @@ import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider defaultTheme={"system"}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      <NextNProgress
+        color="#29D"
+        options={{
+          showSpinner: false,
+        }}
+      />
+
+      <ThemeProvider defaultTheme={"system"}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   );
 }
