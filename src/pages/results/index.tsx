9292db32f404b5e0ced6ba827710f2e1b307f748
RobertTheDev <robertthedev@gmail.com>
2023-12-28 10:59:07 +0000

    feat: added router query

--- src/pages/results/index.tsx
+++ src/pages/results/index.tsx
@@ -1,7 +1,13 @@
+import { useRouter } from "next/router";
+
 export default function ResultsPage() {
+  const router = useRouter();
+  const { q } = router.query;
+
   return (
     <div>
       <h1>Results</h1>
+      <h1>{q}</h1>
     </div>
   );
 }
