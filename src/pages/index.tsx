cc8fd8f146040bbc55807c833324415ef128eca9
RobertTheDev <robertthedev@gmail.com>
2023-12-28 08:57:40 +0000

    feat: added graphql fetch videos query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,46 @@
 import VideoList from "@/components/VideoList";
+import createApolloClient from "@/lib/apolloClient";
+import { gql } from "@apollo/client";
+import { Video } from "@prisma/client";
 
-export default function HomePage() {
+export default function HomePage({ videos }: { videos: Video[] }) {
   return (
     <div>
-      <VideoList />
+      <VideoList videos={videos} />
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const client = createApolloClient();
+  const { data } = await client.query({
+    query: gql`
+      query getVideos {
+        videos {
+          id
+          createdAt
+          title
+          totalComments
+          totalViews
+          channel {
+            id
+            name
+            image {
+              url
+            }
+          }
+          thumbnail {
+            alt
+            url
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      videos: data.videos,
+    },
+  };
 }
